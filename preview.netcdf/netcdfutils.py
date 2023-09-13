57d7300a4bd3b049d52b279204284a3dc89bb81d
toddn <tcnichol@illinois.edu>
2023-09-13 11:52:36 -0500

    saves map for all variables works for NetCDF4 data, not sure about other data sources, will check

--- preview.netcdf/netcdfutils.py
+++ preview.netcdf/netcdfutils.py
@@ -8,43 +8,21 @@ import mpl_toolkits
 from mpl_toolkits.basemap import Basemap
 plt.rcParams['figure.figsize'] = (16.0, 12.0)
 
+
 sample_file = 'ASJP_Year_2023_Day_217.nc4'
-# sample_file = 'pr_2020.nc'
-def main():
-    print('doing files')
-    ds = Dataset(sample_file)
-    variable_names = list(ds.variables.keys())
 
+def generate_mercator_plot_for_variable(current_dataset, current_variable, current_filename):
+    lats = current_dataset.variables['Latitude'][:]
+    lons = current_dataset.variables['Longitude'][:]
 
-    lats = ds.variables['Latitude'][:]
-    lons = ds.variables['Longitude'][:]
-    sample_variable = 'Cloud_Optical_Thickness_Combined_Mean'
-
-
-
-    if sample_variable in variable_names:
-        current_variable = ds.variables[sample_variable]
-
-    current_variable_values = current_variable[:]
+    current_variable_values = current_dataset[current_variable][:]
     current_variable_data = current_variable_values.data
 
-    lon_0 = lons.mean()
-    lat_0 = lats.mean()
+    # mercator projection basemap by default
+    m2 = Basemap(projection='merc', llcrnrlat=-80, urcrnrlat=80,
+                 llcrnrlon=-180, urcrnrlon=180, lat_ts=20, resolution='c')
 
-    # m = Basemap(width=5000000, height=3500000,
-    #             resolution='l', projection='stere',
-    #             lat_ts=40, lat_0=lat_0, lon_0=lon_0)
-
-    # Mercator projection Basemap
-    # m2 = Basemap(projection='merc', llcrnrlat=-80, urcrnrlat=80,
-    #             llcrnrlon=-180, urcrnrlon=180, lat_ts=20, resolution='c')
-
-    # polar stereographic projection basemap
-
-    m2 = Basemap(projection='npstere', boundinglat=10, lon_0=270, resolution='l')
     xi, yi = m2(lons, lats)
-
-
 
     cs2 = m2.pcolor(xi, yi, np.squeeze(current_variable_data))
 
@@ -52,11 +30,23 @@ def main():
     m2.drawcountries()
     m2.drawparallels(np.arange(-90., 91., 30.))
     m2.drawmeridians(np.arange(-180., 181., 60.))
-    plt.savefig('polar.png')
-    # plt.show()
 
-    # Add Colorbar
-    print('here')
+    plot_name = current_filename + '_' + current_variable + '.png'
+    plt.savefig(plot_name)
+    plt.clf()
+
+
+# sample_file = 'pr_2020.nc'
+def main():
+    ds = Dataset(sample_file)
+    variable_names = list(ds.variables.keys())
+
+    for variable in variable_names:
+        if variable != 'Latitude' and variable != 'Longitude':
+            try:
+                generate_mercator_plot_for_variable(current_dataset=ds, current_variable=variable, current_filename=sample_file)
+            except Exception as e:
+                print(e)
 
 if __name__ == "__main__":
     main()
